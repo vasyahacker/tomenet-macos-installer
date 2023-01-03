39a0a40c7493cdef473076b28c3c4723932f7d89
vasyahacker <vasya@magicfreedom.com>
2023-01-03 12:44:07 +0400

    fixes

--- tomenet-macos-installer.sh
+++ tomenet-macos-installer.sh
@@ -35,6 +35,13 @@ INFO_PLIST="<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 </plist>"
 
 RUN_SH='#!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/X11/bin
+
+type Xquartz > /dev/null 2>&1 || {
+ osascript -e "display dialog \"Need Xquartz\" with icon caution buttons {\"Ok\"}"
+ open "https://www.xquartz.org"
+ exit
+}
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do
@@ -44,19 +51,23 @@ while [ -h "$SOURCE" ]; do
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
-[ -e ~/.tomenetrc ] || cp "$DIR/.tomenetrc" ~/
+RC=~/.tomenetrc
+[ -e $RC ] || cp "$DIR/.tomenetrc" ~/
 
-osascript -e "display dialog \"Select TomeNET main window font:\" buttons {\"Default font\", \"Tangars font\"} \
-default button \"Tangars font\" cancel button \"Default font\""  > /dev/null 2>&1 && {
-  /opt/X11/bin/xset fp+ "$DIR/fonts"
-  /opt/X11/bin/xset fp rehash
-  sed -i -e "s/^Mainwindow_Font.*$/Mainwindow_Font 16x22tg/" ~/.tomenetrc
-  true
-} || {
-  sed -i -e "s/^Mainwindow_Font.*tg$/Mainwindow_Font 8x13/" ~/.tomenetrc
+grep -q "^##not first time$" $RC || {
+  echo "##not first time" >> $RC && \
+  osascript -e "display dialog \"Do you prefer graphical-style font?
+You can always change this later in the games options.\" with title \"TomeNET first start question\" \
+buttons {\"Yes\", \"No\"} default button \"Yes\" cancel button \"No\" \
+with icon POSIX file \"$DIR/../Resources/icon.icns\"" > /dev/null 2>&1 && {
+    sed -i -e "s/^Mainwindow_Font.*$/Mainwindow_Font 14x20tg/" $RC
+  }
 }
 
-cd $DIR
+xset fp+ "$DIR/fonts"
+xset fp rehash
+
+cd $DIR || exit
 _arch="$(arch)"
 export DYLD_LIBRARY_PATH="./$_arch"
 ./tomenet-$_arch &
